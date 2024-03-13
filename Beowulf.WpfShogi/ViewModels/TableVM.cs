c6e0e74c485f60dd6803bc7790e8f5ffeb596706
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-13 23:09:35 +0300

    vms

---
+++ Beowulf.WpfShogi/ViewModels/TableVM.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Beowulf.WpfShogi.ViewModels
+{
+    public partial class TableVM : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<CellVM> cellVMs = [];
+
+        [ObservableProperty]
+        public CellVM? curCellVM;
+    }
+}
