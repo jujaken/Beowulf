8d95a2c301cf851c7eb8c15f90b3d7c7f47c010c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-13 22:55:17 +0300

    repos

---
+++ Beowulf.Core/Data/Repos/IRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace Beowulf.Core.Data.Repos
+{
+    public interface IRepo<T> where T : class
+    {
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<T?> Get(Guid id);
+        Task<List<T>> Get(Func<T, bool> condition);
+    }
+}
---
+++ Beowulf.Core/Data/Repos/Repo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Beowulf.Core.Data.Repos
+{
+    public class Repo<T>(BeowulfDbContext context) : IRepo<T> where T : class
+    {
+        private BeowulfDbContext context = context;
+        private DbSet<T> Set => context.Set<T>();
+
+        public async Task Create(T entity)
+        {
+            await Set.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            Set.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            Set.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<T?> Get(Guid id)
+        {
+            return await Set.FindAsync(id);
+        }
+
+        public Task<List<T>> Get(Func<T, bool> condition)
+        {
+            return Task.FromResult(Set.Where(condition).ToList());
+        }
+    }
+}
