b4d3b3c12192cec9015f03ff95399c68dda84537
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-13 22:45:19 +0300

    Add db context

---
+++ Beowulf.Core/Data/BeowulfDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Beowulf.Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beowulf.Core.Data
+{
+    public class BeowulfDbContext : DbContext
+    {
+        public DbSet<CellModel> Cells { get; set; }
+        public DbSet<TableModel> Tables { get; set; }
+    }
+}
