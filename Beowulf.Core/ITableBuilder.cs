b7ef2d3bf01f43e790a1de10106685df60779119
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-15 23:01:22 +0300

    size

--- Beowulf.Core/ITableBuilder.cs
+++ Beowulf.Core/ITableBuilder.cs
@@ -5,6 +5,7 @@ namespace Beowulf.Core
     public interface ITableBuilder
     {
         ITableBuilder WithE(int e);
+        ITableBuilder WithSize(int width, int height);
         ITableBuilder WithCellContentRule(ICellContentRule contentRule, int start, int end);
         ITableBuilder WithCellContentRule(ICellPosRule posRule);
         ITableBuilder WithCellContentRule(ICellLinkRule linkRule);
