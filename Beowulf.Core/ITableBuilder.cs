0707c014859f50180edf164161ab0e9f7c9f47dd
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-15 23:00:19 +0300

    tablebuilder

---
+++ Beowulf.Core/ITableBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Beowulf.Core.Models;
+
+namespace Beowulf.Core
+{
+    public interface ITableBuilder
+    {
+        ITableBuilder WithE(int e);
+        ITableBuilder WithCellContentRule(ICellContentRule contentRule, int start, int end);
+        ITableBuilder WithCellContentRule(ICellPosRule posRule);
+        ITableBuilder WithCellContentRule(ICellLinkRule linkRule);
+        Table Build();
+    }
+}
