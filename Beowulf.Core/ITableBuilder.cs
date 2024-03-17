eee5d1e94fa7ccdf790869a0e5288ab888f9c5df
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-17 20:49:28 +0300

    Red

--- Beowulf.Core/ITableBuilder.cs
+++ Beowulf.Core/ITableBuilder.cs
@@ -1,11 +1,15 @@
-﻿using Beowulf.Core.Models;
+﻿using Beowulf.Core.Data.Models;
+using Beowulf.Core.Data.Repos;
+using Beowulf.Core.Models;
 
 namespace Beowulf.Core
 {
     public interface ITableBuilder
     {
         ITableBuilder WithE(int e);
-        ITableBuilder WithSize(int width, int height);
+        ITableBuilder WithSize(params int[] sizes);
+        ITableBuilder WithTableRepo(IRepo<TableModel> tableRepo);
+        ITableBuilder WithCellRepo(IRepo<CellModel> tableRepo);
         ITableBuilder WithCellContentRule(ICellContentRule contentRule, int start, int end);
         ITableBuilder WithCellContentRule(ICellPosRule posRule);
         ITableBuilder WithCellContentRule(ICellLinkRule linkRule);
