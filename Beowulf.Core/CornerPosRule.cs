e02a0343ce427080e244c773cad4f767ad620f5d
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-27 15:20:58 +0300

    sv

---
+++ Beowulf.Core/CornerPosRule.cs
@@ -0,0 +1,12 @@
+﻿namespace Beowulf.Core
+{
+    public class CornerPosRule(int e = 2) : ICellPosRule
+    {
+        public int E { get; } = e;
+
+        public double[] Next()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
