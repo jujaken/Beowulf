6c47573020340233a628edd25ae85107e01a6a9e
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-03-27 18:17:02 +0300

    test

--- Beowulf.Core/Corner2DPosRule.cs
+++ Beowulf.Core/Corner2DPosRule.cs
@@ -6,9 +6,39 @@ namespace Beowulf.Core
     {
         public int E => 2;
 
+        private bool isX = true;
+
+        private readonly Vector vector = new(0, 0);
+        private int level = 0;
+
         public double[] Next()
         {
-            throw new NotImplementedException();
+            if (vector[0] == level)
+            {
+                if (vector[1] == level)
+                {
+                    level++;
+                    vector[1] = 0;
+                }
+                else
+                {
+                    vector[1] = level;
+                    return vector.Coordinates;
+                }
+            }
+
+            if (isX)
+            {
+                vector[0]++;
+                isX = false;
+            }
+            else
+            {
+                (vector[0], vector[1]) = (vector[1], vector[0]);
+                isX = true;
+            }
+
+            return vector.Coordinates;
         }
     }
 }
